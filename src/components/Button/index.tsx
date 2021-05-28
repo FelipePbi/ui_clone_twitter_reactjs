249fcf4e5747ec4269ee2522e04a1d4923d0b593
Felipe Borges <felipexd-2011@hotmail.com>
2021-05-28 13:05:37 -0300

    refactor: change name props outline to outlined

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 interface Props {
-    outline?: boolean;
+    outlined?: boolean;
 }
 
 export default styled.button<Props>`
-    background: ${props => props.outline ? 'transparent' : 'var(--twitter)'};
-    color: ${props => props.outline ? 'var(--twitter)' : 'var(--white)'};
-    border: ${props => props.outline ? '1px solid var(--twitter)' : 'none'};
+    background: ${props => props.outlined ? 'transparent' : 'var(--twitter)'};
+    color: ${props => props.outlined ? 'var(--twitter)' : 'var(--white)'};
+    border: ${props => props.outlined ? '1px solid var(--twitter)' : 'none'};
 
     padding: 16px;
     border-radius: 25px;
@@ -19,6 +19,6 @@ export default styled.button<Props>`
     outline: 0;
 
     &:hover {
-        background: ${props => props.outline ? 'var(--twitter-dark-hover)' : 'var(--twitter-light-hover)'};
+        background: ${props => props.outlined ? 'var(--twitter-dark-hover)' : 'var(--twitter-light-hover)'};
     }
 `;
