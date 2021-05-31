9ea759b9aaabe2877fd2c74ff03f2c1810ac316e
Felipe Borges <felipexd-2011@hotmail.com>
2021-05-30 22:55:30 -0300

    refactor: add icons to twitter and retwitter

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -13,3 +13,7 @@ export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
 
 export { Chat } from 'styled-icons/heroicons-outline';
+
+export { Retweet } from 'styled-icons/entypo';
+
+export { Twitter } from 'styled-icons/entypo-social';
