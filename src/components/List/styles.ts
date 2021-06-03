bd0631ec888d787402cc8e436792160dd2d948a6
Felipe Borges <felipexd-2011@hotmail.com>
2021-06-03 20:51:33 -0300

    refactor: remove unused variables

--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -1,5 +1,4 @@
-import styled, { css } from 'styled-components';
-import { ArrowLeft, Home, Search, Notifications, Email } from '../../styles/Icons'
+import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
